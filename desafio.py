0fbad4b7dfbbd2cbd53ddce9658f86bae9bb3c01
Matheus Liniker <matheuslinyker22@gmail.com>
2025-09-28 19:20:33 -0300

    version 3

---
+++ desafio.py
@@ -0,0 +1,8 @@
+import pyautogui
+
+pyautogui.moveTo(1475,736, duration=2)
+pyautogui.rightClick()
+pyautogui.move(50, -210, duration=2)
+pyautogui.leftClick()
+pyautogui.move(-50, 0, duration=2)
+pyautogui.leftClick()
